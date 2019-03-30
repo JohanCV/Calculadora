a540691f908598022fd2e92bef880df76ac070b4
Gohan-PC <nahoj1992@gmail.com>
2019-03-30 03:21:02 -0500

    SUMA v1.0.3

--- app/src/main/java/com/jcv/calculadora/Inicio.java
+++ app/src/main/java/com/jcv/calculadora/Inicio.java
@@ -4,6 +4,7 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.DatePicker;
 import android.widget.EditText;
 import android.widget.TextView;
 import android.widget.Toast;
@@ -11,12 +12,14 @@ import android.widget.Toast;
 public class Inicio extends AppCompatActivity {
     public TextView rpta;
     private String operador;
+    private double operadoruno;
+    private double operadordos;
     private Button btn_1;
     private Button btn_limpiador;
     private Button btn_suma;
     private Button btn_igual;
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(final Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_inicio);
 
@@ -26,6 +29,7 @@ public class Inicio extends AppCompatActivity {
         btn_limpiador = findViewById(R.id.btn_limpiar);
         //operaciones
         btn_suma = findViewById(R.id.btn_suma);
+        btn_igual = findViewById(R.id.btn_igual);
 
         btn_1.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -43,14 +47,23 @@ public class Inicio extends AppCompatActivity {
         btn_suma.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
+                String numero = rpta.getText().toString();
+                operadoruno = Double.parseDouble(numero);
+                rpta.setText("+");
                 operador = "+";
             }
         });
         btn_igual.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
+                Toast.makeText(getApplicationContext(),"btn igual",Toast.LENGTH_LONG).show();
+                String numero =rpta.getText().toString();
+                operadordos = Double.parseDouble(numero);
                 if (operador.equals("+")){
-                    int suma = 0;
+                    double suma = 0.0;
+                    suma = operadoruno + operadordos;
+
+                    rpta.setText(String.valueOf(suma));
                 }
             }
         });
